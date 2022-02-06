0eb4842c652226f35caf93ed936058a0a2001916
eilonshi <eilonshi@gmail.com>
2022-02-06 13:40:08 +0200

    added registration and little filtering

--- src/core/vmd.py
+++ src/core/vmd.py
@@ -1,22 +1,46 @@
 import imutils
 import cv2
 import numpy as np
+from image_registration.core.registrar import Registrar
 
 
 class VideoMotionDetector:
-    def __init__(self, cap, history=100, min_size_for_movement=100, var_threshold=16, bluring_kernel_size=21,
-                 display_width=500):
+    def __init__(self, cap, history=100, min_size_for_movement=100, var_threshold=16, blurring_kernel_size=21,
+                 display_width=500, equalize_hist=False, to_reg=True, ignore_border=30):
         self.cap = cap
         self.display_width = display_width
-        self.bluring_kernel_size = bluring_kernel_size
+        self.display_height = 0
+        self.blurring_kernel_size = blurring_kernel_size
         self.min_size_for_movement = min_size_for_movement
         self.back_sub = cv2.createBackgroundSubtractorMOG2(history, var_threshold, False)
+        self.equalize_hist = equalize_hist
+        self.frame_counter = 0
+        self.ignore_border = ignore_border
+        self.history = history
+        self.to_reg = to_reg
+        self.anchor_image = None
+        self.registrar = None
+        self.last_mask = None
 
     def run_video_with_detections(self):
         ret, frame = self.cap.read()
+        self.anchor_image = frame
+        self.registrar = Registrar(point_count=500, top_matches_percent=0.5, is_only_affine=True)
 
         while ret:
+            self.frame_counter += 1
             frame = imutils.resize(frame, width=self.display_width)
+            self.display_height = frame.shape[0]
+
+            if self.equalize_hist:
+                frame_yuv = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+                frame_yuv[:, :, 0] = cv2.equalizeHist(frame_yuv[:, :, 0])
+                frame = cv2.cvtColor(frame_yuv, cv2.COLOR_YUV2BGR)
+
+            if self.to_reg:
+                frame = self.registrar.register_image_by_another_image(image_to_register=frame,
+                                                                       anchor_image=self.anchor_image)
+
             frame, thresh_bgr, fg_mask_bgr, contours = self.get_detections(frame)
             frame = self.draw_contours_on_image(image=frame, contours=contours)
 
@@ -41,16 +65,32 @@ class VideoMotionDetector:
         return image
 
     def get_detections(self, frame):
-        frame_blurred = cv2.GaussianBlur(frame, (self.bluring_kernel_size, self.bluring_kernel_size), 0)
+
+        frame_blurred = cv2.GaussianBlur(frame, (self.blurring_kernel_size, self.blurring_kernel_size), 0)
 
         fg_mask = self.back_sub.apply(frame_blurred)
         fg_mask_bgr = cv2.cvtColor(fg_mask, cv2.COLOR_GRAY2BGR)
 
-        thresh = cv2.erode(fg_mask, None)
-        thresh = cv2.dilate(thresh, None, iterations=5)
-        thresh_bgr = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
-        contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        fg_mask = cv2.erode(fg_mask, None)
+        fg_mask = cv2.dilate(fg_mask, None, iterations=5)
+        fg_morph_bgr = cv2.cvtColor(fg_mask, cv2.COLOR_GRAY2BGR)
 
-        contours = [c for c in contours if cv2.contourArea(c) > self.min_size_for_movement]
+        and_mask = cv2.bitwise_and(self.last_mask, fg_mask) if self.last_mask is not None else fg_mask
+        and_mask = cv2.dilate(and_mask, None, iterations=1)
+        self.last_mask = fg_mask
 
-        return frame, thresh_bgr, fg_mask_bgr, contours
+        contours, _ = cv2.findContours(and_mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        contours = [c for c in contours if cv2.contourArea(c) > self.min_size_for_movement] \
+            if self.frame_counter >= self.history else []
+
+        filtered_contours = []
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
+
+            if x > self.ignore_border and y > self.ignore_border and \
+                    x + w < self.display_width - self.ignore_border and \
+                    y + h < self.display_height - self.ignore_border:
+                filtered_contours.append(contour)
+
+        return frame, fg_morph_bgr, fg_mask_bgr, filtered_contours
--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ from src.core.vmd import VideoMotionDetector
 
 
 def main():
-    cap = cv2.VideoCapture('../data/road.mp4')
-    vmd = VideoMotionDetector(cap=cap)
+    cap = cv2.VideoCapture('../data/vis_87.mp4')
+    vmd = VideoMotionDetector(cap=cap, to_reg=True)
     vmd.run_video_with_detections()
 
 
